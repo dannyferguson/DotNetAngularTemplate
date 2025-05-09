cf75708f59784115eba3bcca8f7667db6dbdfeac
Danny Ferguson <git@todanny.com>
2025-05-09 17:41:04 -0400

    Re-add ip debug endpoint

---
+++ API/Features/Misc/DebugController.cs
@@ -0,0 +1,19 @@
+﻿using DotNetAngularTemplate.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetAngularTemplate.Features.Misc;
+
+[ApiController]
+[Route("api/v1/debug")]
+public class DebugController : ControllerBase
+{
+    [HttpGet]
+    [Route("ip")]
+    public IActionResult Ip()
+    {
+        return Ok(new
+        {
+            ip = IpHelper.GetClientIp(HttpContext)
+        });
+    }
+}
